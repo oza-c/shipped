88ac33370e53512f1923128ddbe65d435ca4d162
Matthes Schlichte <matthes@schlichte.dev>
2022-04-09 19:00:51 +0200

    add "parcel add" page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
+import 'package:shipped/sites/addParcel.dart';
 import 'assets/parcel.dart';
 
 void main() {
@@ -26,40 +27,38 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(196, 226, 133, 19),
-          title: const Text("Shipped - Track your Parcels"),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(196, 226, 133, 19),
-                ),
-                child: Text('Menu'),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(196, 226, 133, 19),
+        title: const Text("Shipped - Track your Parcels"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(196, 226, 133, 19),
               ),
-              ListTile(
-                title: const Text('Item 1'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text(''),
-                onTap: () {},
-              ),
-            ],
-          ),
-        ),
-        body: Padding(
-            padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
-            child: ParcelList()
+              child: Text('Menu'),
             ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text(''),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+          child: ParcelList()),
     );
   }
 }
@@ -71,7 +70,7 @@ class ParcelList extends StatefulWidget {
 
 class _ParcelListState extends State<ParcelList> {
   List<Shipment> data = [];
-    static bool isFabVisible = true;
+  static bool isFabVisible = true;
 
   @override
   void initState() {
@@ -82,77 +81,86 @@ class _ParcelListState extends State<ParcelList> {
 
   Future loadList() async {
     await Future.delayed(const Duration(milliseconds: 1000));
-    data = [];
-    data.add(Shipment(
-        parcelname: 'Amazon',
-        parcel_id: '92183912843023812',
-        shippingprovider: 'DHL'));
-    data.add(Shipment(
-        parcelname: 'LFDY',
-        parcel_id: '92183912843023812',
-        shippingprovider: 'DHL'));
-    data.add(Shipment(
-        parcelname: 'PESO',
-        parcel_id: '92183912843023812',
-        shippingprovider: 'DHL'));
-    data.add(Shipment(
-        parcelname: 'Zalando',
-        parcel_id: '92183912843023812',
-        shippingprovider: 'DHL'));
-
     setState(() => data = data);
+  }
+
+  Future addDataToList(Shipment getData) async {
+    data.add(getData);
+    loadList();
   }
 
   @override
   Widget build(BuildContext context) => buildList();
 
   Widget buildList() => data.isEmpty
-      ? const Center(child: CircularProgressIndicator())
+      ? Center(
+          child: ElevatedButton(
+            onPressed: (() {
+              addNewEntry();
+            }), child: Container(
+              height: 60,
+              width: 200,
+              child: Row(
+              children: const <Widget>[
+                Icon(Icons.add),
+                Text("Add your first Parcel")
+              ]
+            ))))            
       : Scaffold(
-        body: NestedScrollView(
-          floatHeaderSlivers: true,
-          headerSliverBuilder: (context, innerBoxIsScrolled) =>[
-          const SliverAppBar(
-            floating: true,
-            snap : true,
-            title: Text("Add Parcel +"),
-            centerTitle: true,
-            backgroundColor: Color.fromARGB(225, 226, 133, 19),
-          ) 
-          ], body:  LiquidPullToRefresh(
-            onRefresh: loadList,
-            color: const Color.fromARGB(196, 226, 133, 19),
-            key: null,
-            child: NotificationListener<UserScrollNotification>(
-              onNotification: (notification) {
-               if(notification.direction == ScrollDirection.forward) {
-                 if(!isFabVisible) setState(() => isFabVisible = true);
-               }else if(notification.direction == ScrollDirection.reverse){
-                 if(isFabVisible) setState(() => isFabVisible = false);
-               }
-               return true;
-              },
-              child: ListView.separated(
-                shrinkWrap: true,
-                primary: false,
-                padding: const EdgeInsets.all(8),
-                itemCount: data.length,
-                itemBuilder: (context, index) {
-                  return buildItem(data[index]);
+          body: NestedScrollView(
+            floatHeaderSlivers: true,
+            headerSliverBuilder: (context, innerBoxIsScrolled) => [
+              const SliverAppBar(
+                floating: true,
+                snap: true,
+                title: Text("Add Parcel +"),
+                centerTitle: true,
+                backgroundColor: Color.fromARGB(225, 226, 133, 19),
+              )
+            ],
+            body: LiquidPullToRefresh(
+              onRefresh: loadList,
+              color: const Color.fromARGB(196, 226, 133, 19),
+              key: null,
+              child: NotificationListener<UserScrollNotification>(
+                onNotification: (notification) {
+                  if (notification.direction == ScrollDirection.forward) {
+                    if (!isFabVisible) setState(() => isFabVisible = true);
+                  } else if (notification.direction ==
+                      ScrollDirection.reverse) {
+                    if (isFabVisible) setState(() => isFabVisible = false);
+                  }
+                  return true;
                 },
-                separatorBuilder: (BuildContext context, int index) =>
-                    const Divider(),
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  primary: false,
+                  padding: const EdgeInsets.all(8),
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    return buildItem(data[index]);
+                  },
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const Divider(),
+                ),
               ),
             ),
           ),
-        ),
-          floatingActionButton: isFabVisible ? const FloatingActionButton(
-              onPressed: null,
-              backgroundColor: Color.fromARGB(255, 226, 133, 19),
-              child: Icon(Icons.add)
-            )
-            : null,
-      );
+          floatingActionButton: isFabVisible
+              ? FloatingActionButton(
+                  onPressed: () async {
+                    await addNewEntry();
+                  },
+                  backgroundColor: Color.fromARGB(255, 226, 133, 19),
+                  child: Icon(Icons.add))
+              : null,
+        );
+
+  Future<void> addNewEntry() async {
+    final result = await Navigator.push(
+        context, MaterialPageRoute(builder: ((context) => const AddParcel())));
+        if(result != null) addDataToList(result);
+  }
 
   Widget buildItem(Shipment parcel) => Container(
       child: Stack(children: <Widget>[
@@ -195,8 +203,8 @@ class _ParcelListState extends State<ParcelList> {
                 child: const Padding(
                   padding: EdgeInsets.all(8.0),
                   child: Text("Das Paket ist im Zustellzentrum angekommen",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 15)),
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
                 ))),
       ]),
       decoration: const BoxDecoration(
---
+++ lib/sites/addParcel.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:shipped/assets/parcel.dart';
+
+class AddParcel extends StatefulWidget {
+  const AddParcel({Key? key}) : super(key: key);
+
+  @override
+  State<AddParcel> createState() => _AddParcelState();
+}
+
+final _formKey = GlobalKey<FormState>();
+
+class _AddParcelState extends State<AddParcel> {
+  String dropdownValue = 'DHL';
+  TextEditingController _parcelIdController = new TextEditingController();
+  TextEditingController _parcelValueController = new TextEditingController();
+  void validateAndSave() {
+    final form = _formKey.currentState;
+    if (form!.validate()) {
+      Navigator.pop(
+          context,
+          Shipment(
+              parcelname: _parcelIdController.text,
+              parcel_id: _parcelValueController.text,
+              shippingprovider: dropdownValue));
+    } else {
+      print('form is invalid');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(196, 226, 133, 19),
+        title: const Text("Add your Parcel"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(8, 20, 8, 8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                keyboardType: TextInputType.text,
+                autocorrect: false,
+                controller: _parcelIdController,
+                decoration: const InputDecoration(
+                  labelText: 'Sendungsnummer',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Fill the Field please";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: 500,
+                child: DropdownButtonFormField<String>(
+                  decoration: const InputDecoration(
+                    labelText: 'Lieferdienst',
+                    border: OutlineInputBorder(),
+                  ),
+                  value: dropdownValue,
+                  icon: const Icon(Icons.arrow_downward),
+                  elevation: 16,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownValue = newValue!;
+                    });
+                  },
+                  items: <String>['DHL', 'DPD', 'UPS', 'Hermes']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                        value: value,
+                        child: Row(
+                          children: <Widget>[
+                            const Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                              child: Icon(Icons.mail, size: 40),
+                            ),
+                            Padding(
+                                padding: const EdgeInsetsDirectional.fromSTEB(
+                                    20, 5, 0, 0),
+                                child: Text(value,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20)))
+                          ],
+                        ));
+                  }).toList(),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                autocorrect: false,
+                controller: _parcelValueController,
+                decoration: const InputDecoration(
+                  labelText: 'Inhalt',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                  onPressed: () {
+                    validateAndSave();
+                  },
+                  child: const Text("Add Parcel"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
